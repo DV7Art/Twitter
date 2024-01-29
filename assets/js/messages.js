9c8e57a39a726803e33623e9ffa90b06ea5e6bfb
DVArt <dvart@hotmail.com>
2024-01-29 14:22:41 +0200

    update: messages  Changes to be committed: 	modified:   assets/js/messages.js 	modified:   core/ajax/messages.php 	modified:   core/classes/message.php

--- assets/js/messages.js
+++ assets/js/messages.js
@@ -1,9 +1,33 @@
-$(function(){
-    $(document).on('click', '#messagePopup', function(){
-		var getMessages = 1;
-		$.post('http://twitter/core/ajax/messages.php', {showMessage:getMessages}, function(data){
-			$('.popupTweet').html(data);
-			$('#messages').hide();
-  		});
-	});
+$(function () {
+    $(document).on('click', '#messagePopup', function () {
+        var getMessages = 1;
+        $.post('http://twitter/core/ajax/messages.php', { showMessage: getMessages }, function (data) {
+            $('.popupTweet').html(data);
+            $('#messages').hide();
+        });
+    });
+
+    $(document).on('click', '.people-message', function () {
+        var get_id = $(this).data('user');
+        $.post('http://twitter/core/ajax/messages.php', { showChatPopup: get_id }, function (data) {
+            $('.popupTweet').html(data);
+            // if (autoscroll) {
+            //     scrollDown();
+            // }
+            // $('#chat').on('scroll', function () {
+            //     if ($(this).scrollTop() < this.scrollHeight - $(this).height()) {
+            //         autoscroll = false;
+            //     } else {
+            //         autoscroll = true;
+            //     }
+            // });
+            // $('.close-msgPopup').click(function () {
+            //     clearInterval(timer);
+            // });
+        });
+
+        $.post('http://twitter/core/ajax/messages.php', { showChatMessage: get_id }, function (data) {
+            $('.main-msg-inner').html(data);
+        });
+    })
 })
--- core/ajax/messages.php
+++ core/ajax/messages.php
@@ -1,5 +1,12 @@
 <?php
 include '../init.php';
+
+if (isset($_POST['showChatMessage']) && !empty($_POST['showChatMessage'])) {
+    $user_id = $_SESSION['user_id'];
+    $messageFrom = $_POST['showChatMessage'];
+    $getFromM->getMessages($messageFrom, $user_id);
+}
+
 if (isset($_POST['showMessage']) && !empty($_POST['showMessage'])) {
     $user_id = $_SESSION['user_id'];
     $messages = $getFromM->recentMessages($user_id);
@@ -100,6 +107,62 @@ if (isset($_POST['showMessage']) && !empty($_POST['showMessage'])) {
             </div>
         </div>
     </div>
+<?php
+}
+if (isset($_POST['showChatPopup']) && !empty($_POST['showChatPopup'])) {
+    $messageFrom = $_POST['showChatPopup'];
+    $user_id     = $_SESSION['user_id'];
+    $user        = $getFromU->userData($messageFrom);
+?>
+    <div class="popup-message-body-wrap">
+        <input id="popup-message-tweet" type="checkbox" checked="unchecked" />
+        <input id="message-body" type="checkbox" checked="unchecked" />
+        <div class="wrap3">
+            <div class="message-send2">
+                <div class="message-header2">
+                    <div class="message-h-left">
+                        <label class="back-messages" for="mass"><i class="fa fa-angle-left" aria-hidden="true"></i></label>
+                    </div>
+                    <div class="message-h-cen">
+                        <div class="message-head-img">
+                            <img src="<?php echo BASE_URL . $user->profileImage; ?>" />
+                            <h4>Messages</h4>
+                        </div>
+                    </div>
+                    <div class="message-h-right">
+                        <label class="close-msgPopup" for="message-body"><i class="fa fa-times" aria-hidden="true"></i></label>
+                    </div>
+                </div>
+                <div class="message-del">
+                    <div class="message-del-inner">
+                        <h4>Are you sure you want to delete this message? </h4>
+                        <div class="message-del-box">
+                            <span>
+                                <button class="cancel" value="Cancel">Cancel</button>
+                            </span>
+                            <span>
+                                <button class="delete" value="Delete">Delete</button>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="main-msg-wrap">
+                    <div id="chat" class="main-msg-inner">
+
+                    </div>
+                </div>
+                <div class="main-msg-footer">
+                    <div class="main-msg-footer-inner">
+                        <ul>
+                            <li><textarea id="msg" name="msg" placeholder="Write some thing!"></textarea></li>
+                            <li><input id="msg-upload" type="file" value="upload" /><label for="msg-upload"><i class="fa fa-camera" aria-hidden="true"></i></label></li>
+                            <li><input id="send" data-user="<?php echo $messageFrom; ?>" type="submit" value="Send" /></li>
+                        </ul>
+                    </div>
+                </div>
+            </div> <!--MASSGAE send ENDS-->
+        </div> <!--wrap 3 end-->
+    </div><!--POP UP message WRAP END-->
 <?php
 }
 ?>
